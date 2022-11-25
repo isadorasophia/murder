0985a97ed886441ccc52bcf457cf04a5f4a3d395
bonnibel <isaecia@gmail.com>
2022-11-24 22:58:20 -0800

    Start out with effects services...

---
+++ src/Murder/Services/EffectsServices.cs
@@ -0,0 +1,35 @@
+﻿using Bang;
+using Bang.Entities;
+using Murder.Components;
+
+namespace Murder.Services
+{
+    internal static class EffectsServices
+    {
+        /// <summary>
+        /// Add an entity which will apply a "fade-in" effect. Darkening the screen to black.
+        /// </summary>
+        public static void FadeIn(World world, float time)
+        {
+            if (world.TryGetUniqueEntity<FadeScreenComponent>() is not Entity e)
+            {
+                e = world.AddEntity();
+            }
+            
+            e.SetFadeScreen(new(FadeType.In, Game.NowUnescaled, time));
+        }
+
+        /// <summary>
+        /// Add an entity which will apply a "fade-out" effect. Clearing the screeen.
+        /// </summary>
+        public static void FadeOut(World world, float time, float delay = 0)
+        {
+            if (world.TryGetUniqueEntity<FadeScreenComponent>() is not Entity e)
+            {
+                e = world.AddEntity();
+            }
+
+            e.SetFadeScreen(new(FadeType.Out, Game.NowUnescaled + delay, time));
+        }
+    }
+}
---
+++ src/Murder/Services/LevelServices.cs
@@ -0,0 +1,15 @@
+﻿using Bang;
+
+namespace Murder.Services
+{
+    public static class LevelServices
+    {
+        public static ValueTask SwitchScene(Guid nextWorldGuid)
+        {
+            // TODO: Do something fancier?
+            Game.Instance.QueueWorldTransition(nextWorldGuid);
+
+            return default;
+        }
+    }
+}
