24bb878457509339faac133628158417c6d527ab
saint11 <saintjust@gmail.com>
2022-12-10 13:13:34 -0800

    new lerpInt

--- src/Murder/Utilities/Calculator.cs
+++ src/Murder/Utilities/Calculator.cs
@@ -145,6 +145,10 @@ namespace Murder.Utilities
         {
             return origin * (1 - factor) + target * factor;
         }
+        public static int LerpInt(float origin, float target, float factor)
+        {
+            return RoundToInt(origin * (1 - factor) + target * factor);
+        }
 
         public static float LerpSnap(float origin, float target, float factor, float threshold = 0.01f)
         {
