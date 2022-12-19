0874ac0d85647afefd9b1470c818caf2e6a12322
bonnibel <isaecia@gmail.com>
2022-12-19 01:07:59 -0800

    Modified so custom draw is not serialized.

--- src/Murder/Components/Graphics/CustomDrawComponent.cs
+++ src/Murder/Components/Graphics/CustomDrawComponent.cs
@@ -1,8 +1,12 @@
 ﻿using Bang.Components;
+using Murder.Attributes;
 using Murder.Core.Graphics;
+using Murder.Utilities.Attributes;
 
 namespace Murder.Components
 {
+    [RuntimeOnly]
+    [DoNotPersistOnSave]
     public readonly struct CustomDrawComponent : IComponent
     {
         public readonly Action<RenderContext> Draw;
