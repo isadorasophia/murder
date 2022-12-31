a5973e690b13bb369355630cc46d11716e09ed4e
saint11 <saintjust@gmail.com>
2022-12-30 22:15:26 -0800

    debug state machine

---
+++ src/Murder.Editor/Systems/Debug/DebugStateMachineSystem.cs
@@ -0,0 +1,69 @@
+﻿using Bang.Systems;
+using System.Collections.Immutable;
+using Murder.Systems;
+using Bang;
+using Bang.Entities;
+using Bang.StateMachines;
+using System.Reflection;
+
+namespace Road.Editor.Systems
+{
+    /// <summary>
+    /// This is a system used to debug state machine components, by calling the OnStart() method
+    /// on the editor.
+    /// </summary>
+    [DoNotPause]
+    [Filter(typeof(IStateMachineComponent))]
+    [Watch(typeof(IStateMachineComponent))]
+    public class DebugStateMachineSystem : IReactiveSystem
+    {
+        public void OnAdded(World world, ImmutableArray<Entity> entities)
+        {
+            foreach (Entity e in entities)
+            {
+                CallStartMethod(e.GetStateMachine());
+            }
+        }
+
+        public void OnModified(World world, ImmutableArray<Entity> entities)
+        {
+            foreach (Entity e in entities)
+            {
+                CallStartMethod(e.GetStateMachine());
+            }
+        }
+
+        public void OnRemoved(World world, ImmutableArray<Entity> entities) { }
+        
+        private static MethodInfo? _onStartMethod;
+
+        private static FieldInfo GetRoutineField(Type tGenericStateMachine)
+        {
+            FieldInfo? tRoutineField = tGenericStateMachine.GetField("_routine", BindingFlags.NonPublic | BindingFlags.Instance);
+            return tRoutineField!;
+        }
+        
+        private static MethodInfo OnStartMethod()
+        {
+            Type tStateMachcine = typeof(StateMachine);
+            MethodInfo? onStartMethod = tStateMachcine.GetMethod("OnStart", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            return onStartMethod!;
+        }
+
+        /// <summary>
+        /// Horrifying method which will call start on a state machine through
+        /// ~*~reflection~*~.
+        /// This only happens because: I refuse to make this method public.
+        /// </summary>
+        public static void CallStartMethod(IStateMachineComponent stateMachine)
+        {
+            _onStartMethod ??= OnStartMethod();
+            
+            FieldInfo field = GetRoutineField(stateMachine.GetType());
+            StateMachine? routine = field.GetValue(stateMachine) as StateMachine;
+
+            _onStartMethod?.Invoke(routine, parameters: Array.Empty<object>());
+        }
+    }
+}
