300b7b9aea64705205271e143dde56ba9678df04
bonnibel <isaecia@gmail.com>
2023-01-06 19:55:43 -0800

    Fixed some corner cases on interaction rules.

--- src/Murder/Components/InteractionComponents/InteractOnRuleMatchComponent.cs
+++ src/Murder/Components/InteractionComponents/InteractOnRuleMatchComponent.cs
@@ -1,11 +1,38 @@
 ﻿using Bang.Components;
+using Murder.Attributes;
 using Murder.Core.Dialogs;
+using Newtonsoft.Json;
 using System.Collections.Immutable;
 
 namespace Murder.Components
 {
+    public enum AfterInteractRule
+    {
+        InteractOnlyOnce,
+        
+        /// <summary>
+        /// Instead of removing this component once triggered, this will only disable it.
+        /// </summary>
+        InteractOnReload,
+
+        /// <summary>
+        /// Instead of removing this component once triggered, this will remove the entity.
+        /// </summary>
+        RemoveEntity
+    }
+    
     public readonly struct InteractOnRuleMatchComponent : IComponent
     {
+        [Tooltip("Expected behavior once the rule is met.")]
+        public readonly AfterInteractRule AfterInteraction = AfterInteractRule.InteractOnlyOnce;
+
+        /// <summary>
+        /// This will only be triggered once the component has been interacted with.
+        /// Used if <see cref="AfterInteractRule.InteractOnReload"/> is set.
+        /// </summary>
+        [JsonIgnore]
+        public readonly bool Triggered = false;
+
         /// <summary>
         /// List of requirements which will trigger the interactive component within the same entity.
         /// </summary>
@@ -13,6 +40,11 @@ namespace Murder.Components
 
         public InteractOnRuleMatchComponent() { }
 
+        public InteractOnRuleMatchComponent(AfterInteractRule after, bool triggered, ImmutableArray<CriterionNode> requirements) => 
+            (AfterInteraction, Triggered, Requirements) = (after, triggered, requirements);
+
         public InteractOnRuleMatchComponent(params CriterionNode[] criteria) => Requirements = criteria.ToImmutableArray();
+
+        public InteractOnRuleMatchComponent Disable() => new(AfterInteraction, triggered: true, Requirements);
     }
 }
--- src/Murder/Systems/Interactions/InteractOnRuleMatchSystem.cs
+++ src/Murder/Systems/Interactions/InteractOnRuleMatchSystem.cs
@@ -12,6 +12,7 @@ using Murder.Utilities;
 using Murder.Assets;
 using Bang.Interactions;
 using Murder.Interactions;
+using Newtonsoft.Json;
 
 namespace Murder.Systems
 {
@@ -52,6 +53,13 @@ namespace Murder.Systems
 
                 // Match each of its requirements.
                 InteractOnRuleMatchComponent ruleComponent = e.GetInteractOnRuleMatch();
+                if (ruleComponent.Triggered)
+                {
+                    // Skip components that have already been triggered and are persist because *reasons* (probably
+                    // so the save is consistent).
+                    continue;
+                }
+
                 foreach (CriterionNode node in ruleComponent.Requirements)
                 {
                     if (!tracker.Matches(node.Criterion, /* character */ null, world, /* target */ null, out int weight) && 
@@ -63,29 +71,47 @@ namespace Murder.Systems
                     }
                 }
 
-                // If we have a match, trigger the rule and clean up the rule triggers.
-                if (match)
-                {
-                    if (e.HasInteractive())
-                    {
-                        e.SendMessage(new InteractMessage(e));
-                    }
+                bool triggered = false;
 
-                    // No longer watch for rule matches.
-                    e.RemoveInteractOnRuleMatch();
+                // If we have a match, trigger the rule and clean up the rule triggers.
+                if (match && e.HasInteractive())
+                {
+                    e.SendMessage(new InteractMessage(e));
+                    triggered = true;
                 }
 
-                // We do not have an interactive, but if this is actually an *either* component, manually add one.
+                // We do not have an interactive or a match, but if this is actually an *either* component, manually add one.
                 if (e.TryGetPickEntityToAddOnStart() is PickEntityToAddOnStartComponent pickEntityOnStart)
                 {
                     Guid target = match ? pickEntityOnStart.OnMatchPrefab : pickEntityOnStart.OnNotMatchPrefab;
                     
                     e.SetInteractive(new InteractiveComponent<AddEntityOnInteraction>(new(target)));
                     e.SendMessage(new InteractMessage(e));
-
-                    // No longer watch for rule matches.
-                    e.RemoveInteractOnRuleMatch();
+                    triggered = true;
                 }
+                
+                if (triggered)
+                {
+                    CleanupRuleMatchEntity(e, ruleComponent);
+                }
+            }
+        }
+
+        private void CleanupRuleMatchEntity(Entity e, InteractOnRuleMatchComponent rule)
+        {
+            switch (rule.AfterInteraction)
+            {
+                case AfterInteractRule.InteractOnReload:
+                    e.SetInteractOnRuleMatch(rule.Disable());
+                    break;
+
+                case AfterInteractRule.RemoveEntity:
+                    e.Destroy();
+                    break;
+
+                case AfterInteractRule.InteractOnlyOnce:
+                    e.RemoveInteractOnRuleMatch();
+                    break;
             }
         }
     }
