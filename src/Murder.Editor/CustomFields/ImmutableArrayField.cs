f66c4e076a69a5c6ef723c2d7570b010c84e0254
saint11 <saintjust@gmail.com>
2023-02-11 17:56:47 -0800

    list editor improvement

--- src/Murder.Editor/CustomFields/ImmutableArrayField.cs
+++ src/Murder.Editor/CustomFields/ImmutableArrayField.cs
@@ -37,13 +37,59 @@ namespace Murder.Editor.CustomFields
                 ImGui.PushID($"{member.Member.ReflectedType}_{index}");
                 element = elements[index];
 
-                if (ImGuiHelpers.DeleteButton($"delete_{index}"))
-                {
-                    ImGui.PopID();
+                ImGui.BeginGroup();
+                ImGuiHelpers.IconButton('', $"{member.Name}_alter", Game.Data.GameProfile.Theme.Accent);
 
-                    return (true, elements.Remove(element));
+                if (ImGui.IsItemHovered())
+                {
+                    ImGui.OpenPopup($"{member.Member.ReflectedType}_{index}_extras");
+                    ImGui.SetNextWindowPos(ImGui.GetItemRectMin() + new System.Numerics.Vector2(-12,-2));
                 }
 
+                if (ImGui.BeginPopup($"{member.Member.ReflectedType}_{index}_extras"))
+                {
+                    ImGui.BeginGroup();
+                    if (ImGuiHelpers.IconButton('', $"{member.Name}_remove", Game.Data.GameProfile.Theme.HighAccent))
+                    {
+                        elements = elements.RemoveAt(index);
+                        modified = true;
+                    }
+                    ImGuiHelpers.HelpTooltip("Remove");
+
+                    if (ImGuiHelpers.IconButton('', $"{member.Name}_duplicate", Game.Data.GameProfile.Theme.HighAccent))
+                    {
+                        elements = elements.Insert(index, elements[index]);
+                        modified = true;
+                    }
+                    ImGuiHelpers.HelpTooltip("Duplicate");
+
+                    if (ImGuiHelpers.IconButton('', $"{member.Name}_move_up", Game.Data.GameProfile.Theme.HighAccent))
+                    {
+                        if (index > 0)
+                        {
+                            elements = elements.RemoveAt(index).Insert(index - 1, elements[index]);
+                            modified = true;
+                        }
+                    }
+                    ImGuiHelpers.HelpTooltip("Move up");
+
+                    if (ImGuiHelpers.IconButton('', $"{member.Name}_move_down", Game.Data.GameProfile.Theme.HighAccent))
+                    {
+                        if (index < elements.Length - 1)
+                        {
+                            elements = elements.RemoveAt(index).Insert(index + 1, elements[index]);
+                            modified = true;
+                        }
+                    }
+                    ImGuiHelpers.HelpTooltip("Move down");
+
+                    ImGui.EndGroup();
+                    if (!ImGui.IsWindowAppearing() && ImGui.IsWindowFocused() && !ImGui.IsMouseHoveringRect(ImGui.GetWindowPos(), ImGui.GetWindowPos() + ImGui.GetWindowSize()))
+                        ImGui.CloseCurrentPopup();
+                    ImGui.EndPopup();
+
+                }
+                
                 ImGui.SameLine();
 
                 if (DrawElement(ref element, member, index))
