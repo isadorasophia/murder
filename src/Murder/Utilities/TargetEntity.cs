68c751585508d09c7d1d3b21c465cf81c06db2ba
saint11 <saintjust@gmail.com>
2023-07-26 10:12:22 -0700

    target entity enum

---
+++ src/Murder/Utilities/TargetEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Murder.Utilities
+{
+    public enum TargetEntity
+    {
+        Self,
+        Parent
+    }
+}
