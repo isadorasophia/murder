6da295d18bd81573d227e1da16a1631e1c29c40f
saint11 <saintjust@gmail.com>
2023-07-31 19:29:11 -0700

    fade in/out when in area

--- src/Murder/Components/Effects/FadeWhenInAreaComponent.cs
+++ src/Murder/Components/Effects/FadeWhenInAreaComponent.cs
@@ -2,19 +2,25 @@
 using Murder.Utilities.Attributes;
 using System.Collections.Immutable;
 
-namespace Murder.Components
-{
-    /// <summary>
-    /// For now, this is only supported for aseprite components.
-    /// </summary>
-    [Requires(typeof(ColliderComponent))]
-    public readonly struct FadeWhenInAreaComponent : IComponent
-    {
-        public readonly float Duration = 0;
+namespace Murder.Components;
 
-        [Target]
-        public readonly ImmutableArray<string> AppliesTo = ImmutableArray<string>.Empty;
-        
-        public FadeWhenInAreaComponent() { }
+/// <summary>
+/// For now, this is only supported for aseprite components.
+/// </summary>
+[Requires(typeof(ColliderComponent))]
+public readonly struct FadeWhenInAreaComponent : IComponent
+{
+    public enum FadeWhenInAreaStyle
+    {
+        HideWhenInArea,
+        ShowWhenInArea
     }
+
+    public readonly float Duration = 0;
+    public readonly FadeWhenInAreaStyle Style = FadeWhenInAreaStyle.HideWhenInArea;
+
+    [Target]
+    public readonly ImmutableArray<string> AppliesTo = ImmutableArray<string>.Empty;
+    
+    public FadeWhenInAreaComponent() { }
 }
