088f2a956991bd7f6b2d8b66964047539c8e0d95
bonnibel <isaecia@gmail.com>
2023-09-17 00:14:29 -0700

    Fixed one (1) warning.

--- src/Murder/Messages/Physics/CollidedWithMessage.cs
+++ src/Murder/Messages/Physics/CollidedWithMessage.cs
@@ -1,23 +1,17 @@
 ﻿using Bang.Components;
 using Murder.Core.Geometry;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Murder.Messages
 {
     
     public readonly struct CollidedWithMessage : IMessage
     {
-
         public readonly Vector2 Pushout;
         public readonly int EntityId;
+
         /// <summary>
         /// Signals a collision with another entity
         /// </summary>
-        /// <param name="entityId">The scene ID of the other entity</param>
         public CollidedWithMessage(int entityId, Core.Geometry.Vector2 pushout)
         {
             EntityId = entityId;
