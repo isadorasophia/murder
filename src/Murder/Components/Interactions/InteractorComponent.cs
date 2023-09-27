77d70de52b75a0259d78d2a2a786600e97cdd890
bonnibel <isaecia@gmail.com>
2023-09-27 00:36:14 -0700

    Rename interaction components.

---
+++ src/Murder/Components/Interactions/InteractorComponent.cs
@@ -0,0 +1,9 @@
+﻿using Bang.Components;
+
+namespace Murder.Interactions
+{
+    /// <summary>
+    /// Component used to signal that an entity is able to interact with other objects.
+    /// </summary>
+    public readonly struct InteractorComponent : IComponent { }
+}
