a8ac2f91d06156b574a3c50b25556296c9b9a630
bonnibel <isaecia@gmail.com>
2023-10-02 14:56:58 -0700

    Fix such that editor systems no longer pick up in world.

---
+++ src/Murder.Editor/Importers/AsepriteImporter_Preload.cs
@@ -0,0 +1,19 @@
+﻿using Murder.Data;
+using Murder.Editor.Assets;
+
+namespace Murder.Editor.Importers
+{
+    [ImporterSettings(FilterType.OnlyTheseFolders, new[] { RelativeDirectory }, new[] { ".aseprite", ".ase" })]
+    internal class PreloadAsepriteImporter(EditorSettingsAsset editorSettings) : AsepriteImporter(editorSettings)
+    {
+        protected override AtlasId Atlas => AtlasId.Preload;
+
+        private const string RelativeDirectory = "preload_images";
+
+        public override string RelativeSourcePath => RelativeDirectory;
+
+        public override string RelativeOutputPath => string.Empty;
+
+        public override string RelativeDataOutputPath => string.Empty;
+    }
+}
