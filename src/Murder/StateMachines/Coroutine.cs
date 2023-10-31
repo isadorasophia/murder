4b4340c15b03f03ca6c09ef5c433f7c5e920d88b
bonnibel <isaecia@gmail.com>
2023-10-30 20:56:20 -0700

    Add a bad idea warning on coroutine.

--- src/Murder/StateMachines/Coroutine.cs
+++ src/Murder/StateMachines/Coroutine.cs
@@ -3,6 +3,9 @@ using Murder.Utilities.Attributes;
 
 namespace Murder.StateMachines
 {
+    /// <summary>
+    /// This CANNOT and WONT be serialized it is just a bad idea. Remember, we can't (or don't want to) serialize lambdas.
+    /// </summary>
     [RuntimeOnly]
     public class Coroutine : StateMachine
     {
