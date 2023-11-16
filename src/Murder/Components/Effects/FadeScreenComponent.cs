927c793ac8f37ec57c583a23f1c6048663d47d28
saint11 <saintjust@gmail.com>
2023-11-16 00:40:25 -0800

    fade now auto-destroys

--- src/Murder/Components/Effects/FadeScreenComponent.cs
+++ src/Murder/Components/Effects/FadeScreenComponent.cs
@@ -24,22 +24,18 @@ namespace Murder.Components
 
         public readonly string CustomTexture;
 
-        public readonly bool DestroyAfterFinished;
-
         public readonly float Sorting = 0;
 
         /// <summary>
         /// Fades the screen using the FadeScreenSystem. Duration will be a minimum of 0.1
         /// </summary>
-        public FadeScreenComponent(FadeType fade, float startedTime, float duration, Color color,
-            bool destroyAfterFinished = false, string customTexture = "", float sorting = 0)
+        public FadeScreenComponent(FadeType fade, float startedTime, float duration, Color color, string customTexture = "", float sorting = 0)
         {
             StartedTime = startedTime;
             Duration = MathF.Max(duration - 0.1f, 0.1f);
             Fade = fade;
             Color = color;
             CustomTexture = customTexture;
-            DestroyAfterFinished = destroyAfterFinished;
             Sorting = sorting;
         }
     }
--- src/Murder/Services/EffectsServices.cs
+++ src/Murder/Services/EffectsServices.cs
@@ -12,19 +12,24 @@ namespace Murder.Services
         /// <summary>
         /// Add an entity which will apply a "fade-in" effect. Darkening the screen to black.
         /// </summary>
-        public static void FadeIn(World world, float time, Color color, bool destroyAfterFinished = true, float sorting = 0)
+        public static void FadeIn(World world, float time, Color color, float sorting = 0)
         {
             var e = world.AddEntity();
-            e.SetFadeScreen(new(FadeType.In, Game.NowUnscaled, time, color, destroyAfterFinished, sorting: sorting));
+            e.SetFadeScreen(new(FadeType.In, Game.NowUnscaled, time, color, sorting: sorting));
         }
 
         /// <summary>
-        /// Add an entity which will apply a "fade-out" effect. Clearing the screeen.
+        /// Add an entity which will apply a "fade-out" effect. Clearing the screen.
         /// </summary>
-        public static void FadeOut(World world, float time, Color color, float delay = 0, bool destroyAfterFinished = true)
+        public static void FadeOut(World world, float time, Color color, float delay = 0)
         {
+            foreach (var old in world.GetEntitiesWith(typeof(FadeScreenComponent)))
+            {
+                old.Destroy();
+            }
+
             var e = world.AddEntity();
-            e.SetFadeScreen(new(FadeType.Out, Game.NowUnscaled + delay, time, color, destroyAfterFinished));
+            e.SetFadeScreen(new(FadeType.Out, Game.NowUnscaled + delay, time, color));
         }
 
         public static void ApplyHighlight(World world, Entity e, HighlightSpriteComponent highlight)
