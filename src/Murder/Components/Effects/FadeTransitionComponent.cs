43a6eed99df7c4dedecd339cc906d362b58df128
saint11 <saintjust@gmail.com>
2023-11-26 21:18:35 -0800

    made sure fadetransition is runtime only

--- src/Murder/Components/Effects/FadeTransitionComponent.cs
+++ src/Murder/Components/Effects/FadeTransitionComponent.cs
@@ -1,37 +1,39 @@
 ﻿using Bang.Components;
+using Murder.Attributes;
+using Murder.Utilities.Attributes;
 using Newtonsoft.Json;
 
-namespace Murder.Components
+namespace Murder.Components;
+
+/// <summary>
+/// For now, this will only fade out aseprite components.
+/// </summary>
+[RuntimeOnly]
+public readonly struct FadeTransitionComponent : IComponent
 {
+    [JsonIgnore]
+    public readonly float StartTime;
+
     /// <summary>
-    /// For now, this will only fade out aseprite components.
+    /// Fade duration in seconds.
     /// </summary>
-    public readonly struct FadeTransitionComponent : IComponent
+    public readonly float Duration;
+
+    public readonly float StartAlpha;
+
+    public readonly float TargetAlpha;
+
+    public readonly bool DestroyEntityOnEnd;
+
+    public FadeTransitionComponent(float duration, float startAlpha, float targetAlpha) : this(duration, startAlpha, targetAlpha, destroyOnEnd: false) { }
+
+    public FadeTransitionComponent(float duration, float startAlpha, float targetAlpha, bool destroyOnEnd)
     {
-        [JsonIgnore]
-        public readonly float StartTime;
+        StartTime = Game.NowUnscaled;
 
-        /// <summary>
-        /// Fade duration in seconds.
-        /// </summary>
-        public readonly float Duration;
-
-        public readonly float StartAlpha;
-
-        public readonly float TargetAlpha;
-
-        public readonly bool DestroyEntityOnEnd;
-
-        public FadeTransitionComponent(float duration, float startAlpha, float targetAlpha) : this(duration, startAlpha, targetAlpha, destroyOnEnd: false) { }
-
-        public FadeTransitionComponent(float duration, float startAlpha, float targetAlpha, bool destroyOnEnd)
-        {
-            StartTime = Game.NowUnscaled;
-
-            StartAlpha = startAlpha;
-            TargetAlpha = targetAlpha;
-            Duration = duration;
-            DestroyEntityOnEnd = destroyOnEnd;
-        }
+        StartAlpha = startAlpha;
+        TargetAlpha = targetAlpha;
+        Duration = duration;
+        DestroyEntityOnEnd = destroyOnEnd;
     }
 }
