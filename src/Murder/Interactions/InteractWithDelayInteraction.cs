fac5636a6aaa4dd0f15e74a1ed5650a545349310
bonnibel <isaecia@gmail.com>
2024-01-28 01:29:19 -0800

    Add InteractWithDelayInteraction.

---
+++ src/Murder/Interactions/InteractWithDelayInteraction.cs
@@ -0,0 +1,28 @@
+﻿using Bang;
+using Bang.Components;
+using Bang.Entities;
+using Bang.Interactions;
+using Bang.StateMachines;
+using Murder.Services;
+
+namespace Murder.Interaction
+{
+    internal readonly struct InteractWithDelayInteraction : IInteraction
+    {
+        public readonly float Time;
+
+        public readonly IInteraction Interactive;
+
+        public void Interact(World world, Entity interactor, Entity? interacted)
+        {
+            world.RunCoroutine(WaitBeforeInteract(world, interactor, interacted, Time, Interactive));
+        }
+
+        private static IEnumerator<Wait> WaitBeforeInteract(
+            World world, Entity interactor, Entity? interacted, float time, IInteraction interactive)
+        {
+            yield return Wait.ForSeconds(time);
+            interactive.Interact(world, interactor, interacted);
+        }
+    }
+}
