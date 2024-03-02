79934bee9b2de9bc0032205777ca43ce62c834d4
bonnibel <isaecia@gmail.com>
2024-03-02 01:45:44 -0800

    Super nit, just removed some usings.

--- src/Murder/Core/Graphics/AnimationEventMessage.cs
+++ src/Murder/Core/Graphics/AnimationEventMessage.cs
@@ -1,11 +1,6 @@
 ﻿using Bang.Components;
 using Murder.Attributes;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Murder.Core.Graphics
 {
