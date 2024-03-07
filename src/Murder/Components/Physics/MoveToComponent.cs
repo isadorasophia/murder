45eae202ed28e59d6a8a8cc381a3fa1ebb4ec6aa
bonnibel <isaecia@gmail.com>
2024-03-07 01:24:00 -0800

    Change default MoveTo distance.

--- src/Murder/Components/Physics/MoveToComponent.cs
+++ src/Murder/Components/Physics/MoveToComponent.cs
@@ -8,7 +8,7 @@ namespace Murder.Components
     public readonly struct MoveToComponent : IComponent
     {
         public readonly Vector2 Target;
-        public readonly float MinDistance = 1;
+        public readonly float MinDistance = 2;
         public readonly float SlowDownDistance = 12;
 
         public MoveToComponent(in Vector2 target)
