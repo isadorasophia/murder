05fa84275217d381802e6809d4e736392fe1ee2f
William Barbosa <heytherewill@gmail.com>
2024-04-08 08:06:35 +0200

    🔙 Minimal Undo Stack

---
+++ src/Murder.Editor/Undo_Implementations.cs
@@ -0,0 +1,56 @@
+﻿namespace Murder.Editor;
+
+public interface IUndoableAction
+{
+    void Perform();
+    void Undo();
+}
+
+public sealed class SimpleUndoableAction(
+    Action perform,
+    Action undo
+) : IUndoableAction
+{
+    public void Perform()
+    {
+        perform();
+    }
+
+    public void Undo()
+    {
+        undo();
+    }
+}
+
+public sealed class BooleanUndoableAction(
+    bool newValue,
+    Action<bool> action
+) : IUndoableAction
+{
+    public void Perform()
+    {
+        action(newValue);
+    }
+
+    public void Undo()
+    {
+        action(!newValue);
+    }
+}
+
+public sealed class IntegerUndoableAction(
+    int currentValue,
+    int newValue,
+    Action<int> action
+) : IUndoableAction
+{
+    public void Perform()
+    {
+        action(currentValue);
+    }
+
+    public void Undo()
+    {
+        action(newValue);
+    }
+}
