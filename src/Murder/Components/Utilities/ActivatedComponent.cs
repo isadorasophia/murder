c44b94883f7d94aa951738ba785481099506457b
bonnibel <isaecia@gmail.com>
2024-06-02 02:26:17 -0700

    Add utility components.

---
+++ src/Murder/Components/Utilities/ActivatedComponent.cs
@@ -0,0 +1,10 @@
+﻿using Bang.Components;
+
+namespace Murder.Components;
+
+/// <summary>
+/// Usually if, for some reason or another, this entity is activated.
+/// </summary>
+public readonly struct ActivatedComponent : IComponent
+{
+}
---
+++ src/Murder/Components/Utilities/IgnoreComponent.cs
@@ -0,0 +1,10 @@
+﻿using Bang.Components;
+
+namespace Murder.Components;
+
+/// <summary>
+/// Usually if, for some reason or another, this entity should be ignored.
+/// </summary>
+public readonly struct IgnoreComponent : IComponent
+{
+}
