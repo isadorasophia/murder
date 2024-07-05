da28b733cc15aebd32bca60ee949c8a8a32581a0
bonnibel <isaecia@gmail.com>
2024-07-05 00:29:49 -0700

    Add new type for tracking world entities out of the world.

---
+++ src/Murder.Editor/CustomFields/WorldAssetEntityInfoField.cs
@@ -0,0 +1,35 @@
+﻿using ImGuiNET;
+using Murder.Assets;
+using Murder.Editor.CustomFields;
+using Murder.Editor.ImGuiExtended;
+using Murder.Editor.Reflection;
+
+namespace Murder.Editor;
+
+[CustomFieldOf(typeof(WorldAssetEntityInfo))]
+public class WorldAssetEntityInfoField : CustomField
+{
+    public override (bool modified, object? result) ProcessInput(EditorMember member, object? fieldValue)
+    {
+        bool modified = false;
+        WorldAssetEntityInfo info = (WorldAssetEntityInfo)fieldValue!;
+
+        modified |= DrawValueWithId(ref info, nameof(WorldAssetEntityInfo.World));
+
+        if (info.World != Guid.Empty && Game.Data.TryGetAsset<WorldAsset>(info.World) is WorldAsset world)
+        {
+            Guid entityGuid = info.Entity;
+            if (SearchBox.SearchInstanceInWorld(ref entityGuid, world))
+            {
+                info = info with { Entity = entityGuid };
+                modified = true;
+            }
+        }
+        else
+        {
+            ImGui.TextColored(Game.Profile.Theme.Faded, "Select a world guid before choosing an entity!");
+        }
+
+        return (modified, info);
+    }
+}
---
+++ src/Murder/Assets/Editor/WorldAssetEntityInfo.cs
@@ -0,0 +1,13 @@
+﻿using Murder.Assets;
+using Murder.Attributes;
+
+namespace Murder.Assets;
+
+public readonly struct WorldAssetEntityInfo
+{
+    [GameAssetId<WorldAsset>]
+    public Guid World { get; init; } = Guid.Empty;
+    public Guid Entity { get; init; } = Guid.Empty;
+
+    public WorldAssetEntityInfo() { }
+}
