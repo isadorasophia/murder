77f9b8d9e27d05196adae14fb0740fd93f9767bb
bonnibel <isaecia@gmail.com>
2024-07-31 00:25:44 -0700

    Add new messages.

--- src/Murder/Interactions/SendMessageInteraction.cs
+++ src/Murder/Interactions/SendMessageInteraction.cs
@@ -2,19 +2,52 @@
 using Bang.Components;
 using Bang.Entities;
 using Bang.Interactions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Murder.Diagnostics;
+using Murder.Utilities;
 
 namespace Murder.Interactions;
 
 public readonly struct SendMessageInteraction : IInteraction
 {
-    public readonly IMessage Message;
+    public readonly IMessage? Message = null;
+
+    public readonly TargetEntity Target = TargetEntity.Self;
+
+    public SendMessageInteraction() { }
+
     public void Interact(World world, Entity interactor, Entity? interacted)
     {
-        interacted?.SendMessage(Message);
+        if (interacted is null || Message is null)
+        {
+            return; 
+        }
+
+        Entity? target;
+        switch (Target)
+        {
+            case TargetEntity.Self:
+                target = interacted;
+                break;
+
+            case TargetEntity.Parent:
+                target = interacted.TryFetchParent();
+                break;
+
+            case TargetEntity.Interactor:
+                target = interactor;
+                break;
+
+            case TargetEntity.Target:
+                int entityId = interacted.TryGetIdTarget()?.Target ?? -1;
+                target = world.TryGetEntity(entityId);
+                break;
+
+            default:
+                GameLogger.Error("Unsupported TargetEntity for SendMessageInteraction.");
+                target = null;
+                break;
+        }
+
+        target?.SendMessage(Message);
     }
 }
