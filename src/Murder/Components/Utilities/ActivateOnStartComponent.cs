8b8d9b4a8b626dc750be05aed5374ab489d2de3e
saint11 <saintjust@gmail.com>
2024-09-15 16:51:39 -0700

    activate on start system

---
+++ src/Murder/Components/Utilities/ActivateOnStartComponent.cs
@@ -0,0 +1,14 @@
+﻿using Bang.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Murder.Components.Utilities
+{
+    public readonly struct ActivateOnStartComponent : IComponent
+    {
+        public readonly bool DeactivateInstead;
+    }
+}
---
+++ src/Murder/Systems/ActivateOnStartSystem.cs
@@ -0,0 +1,26 @@
+﻿using Bang.Contexts;
+using Bang.Entities;
+using Bang.Systems;
+using Murder.Components.Utilities;
+
+namespace Murder.Systems;
+
+[Filter(ContextAccessorFilter.AllOf, typeof(ActivateOnStartComponent))]
+public class ActivateOnStartSystem : IStartupSystem
+{
+    public void Start(Context context)
+    {
+        foreach (var e in context.Entities)
+        {
+            var activate = e.GetActivateOnStart();
+            if (activate.DeactivateInstead)
+            {
+                e.Deactivate();
+            }
+            else
+            {
+                e.Activate();
+            }
+        }
+    }
+}
