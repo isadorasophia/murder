12a8348eb326ea737308d2203c4349f4f7237d6b
bonnibel <isaecia@gmail.com>
2024-10-04 20:09:33 -0700

    Nit change on disable transition effects.

--- src/Murder/Components/World/DisableSceneTransitionEffectsComponent.cs
+++ src/Murder/Components/World/DisableSceneTransitionEffectsComponent.cs
@@ -2,19 +2,18 @@
 using Murder.Utilities.Attributes;
 using System.Numerics;
 
-namespace Murder.Components
+namespace Murder.Components;
+
+/// <summary>
+/// Component that will disable any transition effects between scenes.
+/// </summary>
+[RuntimeOnly]
+[Unique]
+public readonly struct DisableSceneTransitionEffectsComponent : IComponent
 {
-    /// <summary>
-    /// Component that will disable any transition effects between scenes.
-    /// </summary>
-    [RuntimeOnly]
-    [Unique]
-    public readonly struct DisableSceneTransitionEffectsComponent : IComponent
-    {
-        public readonly Vector2? OverrideCameraPosition = null;
+    public readonly Vector2? OverrideCameraPosition = null;
 
-        public DisableSceneTransitionEffectsComponent() { }
+    public DisableSceneTransitionEffectsComponent() { }
 
-        public DisableSceneTransitionEffectsComponent(Vector2 bounds) => OverrideCameraPosition = bounds;
-    }
+    public DisableSceneTransitionEffectsComponent(Vector2 bounds) => OverrideCameraPosition = bounds;
 }
