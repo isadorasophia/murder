f3ae8d61ee033e26ac0cb7e8f6f81d38f3e35e76
bonnibel <isaecia@gmail.com>
2024-12-16 15:16:10 -0800

    Support debugger display on criterions

--- src/Murder/Core/Dialogs/CriterionNode.cs
+++ src/Murder/Core/Dialogs/CriterionNode.cs
@@ -1,5 +1,9 @@
-﻿namespace Murder.Core.Dialogs
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace Murder.Core.Dialogs
 {
+    [DebuggerDisplay("{DebuggerDisplay(),nq}")]
     public readonly struct CriterionNode
     {
         public readonly Criterion Criterion = new();
@@ -16,5 +20,37 @@
         public CriterionNode WithCriterion(Criterion criterion) => new(criterion, Kind);
 
         public CriterionNode WithKind(CriterionNodeKind kind) => new(Criterion, kind);
+
+        public string DebuggerDisplay()
+        {
+            StringBuilder result = new();
+
+            result.Append($"{Criterion.Fact.Name} {Criterion.Kind} ");
+
+            switch (Criterion.Fact.Kind)
+            {
+                case FactKind.Bool:
+                    result = result.Append(Criterion.BoolValue);
+                    break;
+
+                case FactKind.Int:
+                    result = result.Append(Criterion.IntValue);
+                    break;
+
+                case FactKind.String:
+                    result = result.Append(Criterion.StrValue);
+                    break;
+
+                case FactKind.Weight:
+                    result = result.Append(Criterion.IntValue);
+                    break;
+
+                case FactKind.Float:
+                    result = result.Append(Criterion.FloatValue);
+                    break;
+            }
+
+            return result.ToString();
+        }
     }
 }
