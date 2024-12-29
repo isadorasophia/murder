a45c80725407d14751927fc65d7cd4697d774bbf
bonnibel <isaecia@gmail.com>
2024-12-28 19:51:53 -0800

    Support running coroutines on pause.

--- src/Murder/Services/CoroutineServices.cs
+++ src/Murder/Services/CoroutineServices.cs
@@ -3,26 +3,36 @@ using Bang.Entities;
 using Bang.StateMachines;
 using Murder.StateMachines;
 
-namespace Murder.Services
+namespace Murder.Services;
+
+public enum CoroutineFlags
 {
-    public static class CoroutineServices
+    None = 0,
+    DoNotPause = 1
+}
+
+public static class CoroutineServices
+{
+    public static void RunCoroutine(this World world, IEnumerator<Wait> routine, CoroutineFlags flags = CoroutineFlags.None)
     {
-        public static void RunCoroutine(this World world, IEnumerator<Wait> routine)
-        {
-            // TODO: Figure out object pulling of entities here.
-            Entity e = world.AddEntity(
-                new StateMachineComponent<Coroutine>(new Coroutine(routine)));
+        // TODO: Figure out object pulling of entities here.
+        Entity e = world.AddEntity(
+            new StateMachineComponent<Coroutine>(new Coroutine(routine)));
 
-            // Immediately run the first tick!
-            e.GetStateMachine().Tick(Game.DeltaTime);
+        if (flags.HasFlag(CoroutineFlags.DoNotPause))
+        {
+            e.SetDoNotPause();
         }
 
-        public static void RunCoroutine(this Entity e, IEnumerator<Wait> routine)
-        {
-            e.SetStateMachine(new StateMachineComponent<Coroutine>(new Coroutine(routine)));
+        // Immediately run the first tick!
+        e.GetStateMachine().Tick(Game.DeltaTime);
+    }
 
-            // Immediately run the first tick!
-            e.GetStateMachine().Tick(Game.DeltaTime);
-        }
+    public static void RunCoroutine(this Entity e, IEnumerator<Wait> routine)
+    {
+        e.SetStateMachine(new StateMachineComponent<Coroutine>(new Coroutine(routine)));
+
+        // Immediately run the first tick!
+        e.GetStateMachine().Tick(Game.DeltaTime);
     }
 }
