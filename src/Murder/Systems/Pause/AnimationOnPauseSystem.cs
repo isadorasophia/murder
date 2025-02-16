f6b8609b71a91291817cd30776fe19363198812e
bonnibel <isaecia@gmail.com>
2025-02-15 23:34:53 -0800

    Send empty animation events when skipping cutscenes.

--- src/Murder/Systems/Pause/AnimationOnPauseSystem.cs
+++ src/Murder/Systems/Pause/AnimationOnPauseSystem.cs
@@ -28,11 +28,17 @@ namespace Murder.Systems
             foreach (Entity e in context.World.GetEntitiesWith(typeof(SpriteComponent)))
             {
                 RenderServices.MessageCompleteAnimations(e);
+                
+                // some cutscenes depend on this to proceed
+                e.SendAnimationEventMessage(string.Empty);
             }
 
             foreach (Entity e in context.World.GetEntitiesWith(typeof(AgentSpriteComponent)))
             {
                 RenderServices.MessageCompleteAnimations(e);
+
+                // some cutscenes depend on this to proceed
+                e.SendAnimationEventMessage(string.Empty);
             }
         }
     }
