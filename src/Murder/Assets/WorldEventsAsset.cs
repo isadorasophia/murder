9db085ac78130ff3ee4233b401b1433c228b4eb1
bonnibel <isaecia@gmail.com>
2025-02-21 17:03:27 -0800

    Nit changes in WorldEventsAssets.

--- src/Murder/Assets/WorldEventsAsset.cs
+++ src/Murder/Assets/WorldEventsAsset.cs
@@ -1,20 +1,18 @@
-﻿using Murder.Assets;
-using Murder.Attributes;
+﻿using Murder.Attributes;
 using Murder.Core;
 using Murder.Utilities;
 using System.Collections.Immutable;
 using System.Numerics;
 
-namespace Murder.Assets
+namespace Murder.Assets;
+
+public class WorldEventsAsset : GameAsset
 {
-    public class WorldEventsAsset : GameAsset
-    {
-        public override char Icon => '\uf7c0';
-        public override string EditorFolder => "#\uf7c0Global Events";
+    public override char Icon => '\uf7c0';
+    public override string EditorFolder => "#\uf7c0Global Events";
 
-        public override Vector4 EditorColor => "#34ebcf".ToVector4Color();
+    public override Vector4 EditorColor => "#34ebcf".ToVector4Color();
 
-        [Tooltip("Events that will be placed on all maps (or the specified one)")]
-        public ImmutableArray<TriggerEventOn> Watchers = ImmutableArray<TriggerEventOn>.Empty;
-    }
+    [Tooltip("Events that will be placed on all maps (or the specified one)")]
+    public ImmutableArray<TriggerEventOn> Watchers = [];
 }
