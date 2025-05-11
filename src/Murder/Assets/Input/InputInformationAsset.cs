e3afb43f430c37290701c893fd44c5b71df7a623
saint11 <saintjust@gmail.com>
2025-05-11 15:26:05 -0700

    cleaning up input info asset

--- src/Murder/Assets/Input/InputInformationAsset.cs
+++ src/Murder/Assets/Input/InputInformationAsset.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework.Input;
+using Murder.Assets.Graphics;
 using Murder.Attributes;
 using Murder.Core;
 using Murder.Core.Input;
@@ -19,137 +20,10 @@ public class InputInformationAsset : GameAsset
     public ImmutableArray<InputInformation> Buttons = ImmutableArray<InputInformation>.Empty;
     public ImmutableArray<ButtonGraphics> Graphics = ImmutableArray<ButtonGraphics>.Empty;
 
-    private ImmutableDictionary<int, InputInformation>? _buttonsCache = null;
-    private ImmutableDictionary<Keys, ButtonGraphics>? _graphicsCacheKeys = null;
-    private ImmutableDictionary<Buttons, ButtonGraphics>? _graphicsCacheButtons = null;
-    private ImmutableDictionary<MouseButtons, ButtonGraphics>? _graphicsCacheMouseButtons = null;
-    private ImmutableDictionary<GamepadAxis, ButtonGraphics>? _graphicsCacheGamepadAxis = null;
-
-    public InputInformation? GetButtonInfo(int id)
-    {
-        if (_buttonsCache == null)
-        {
-            _buttonsCache = BuildButtonsCache();
-        }
-
-        if (_buttonsCache.TryGetValue(id, out InputInformation buttonInfo))
-        {
-            return buttonInfo;
-        }
-
-        return null;
-    }
-
-    private ImmutableDictionary<int, InputInformation> BuildButtonsCache()
-    {
-        var builder = ImmutableDictionary.CreateBuilder<int, InputInformation>();
-        foreach (var info in Buttons)
-        {
-            builder.Add(info.ButtonId, info);
-        }
-
-        return builder.ToImmutable();
-    }
-
-    public (Portrait?, string?) GetGraphicsFor(InputButton key)
-    {
-        if (_graphicsCacheKeys == null)
-        {
-            BuildGraphicsCache();
-        }
-
-        switch (key.Source)
-        {
-            case InputSource.Keyboard:
-                if (key.Keyboard is Keys keyCode && _graphicsCacheKeys!.TryGetValue(keyCode, out var graphics))
-                {
-                    return (graphics.Icon, graphics.Text);
-                }
-                return (null, null);
-            case InputSource.Mouse:
-                if (key.Mouse is MouseButtons mouseButton && _graphicsCacheMouseButtons!.TryGetValue(mouseButton, out var mouseGraphics))
-                {
-                    return (mouseGraphics.Icon, mouseGraphics.Text);
-                }
-                return (null, null);
-            case InputSource.Gamepad:
-                if (key.Gamepad is Buttons button && _graphicsCacheButtons!.TryGetValue(button, out var buttonGraphics))
-                {
-                    return (buttonGraphics.Icon, buttonGraphics.Text);
-                }
-                return (null, null);
-            case InputSource.GamepadAxis:
-                if (key.Axis is GamepadAxis axis && _graphicsCacheGamepadAxis!.TryGetValue(axis, out var axisGraphics))
-                {
-                    return (axisGraphics.Icon, axisGraphics.Text);
-                }
-                return (null, null);
-            default:
-                return (null, null);
-        }
-    }
-
-    private void BuildGraphicsCache()
-    {
-        var keysBuilder = ImmutableDictionary.CreateBuilder<Keys, ButtonGraphics>();
-        var buttonsBuilder = ImmutableDictionary.CreateBuilder<Buttons, ButtonGraphics>();
-        var mouseButtonsBuilder = ImmutableDictionary.CreateBuilder<MouseButtons, ButtonGraphics>();
-        var gamepadAxisBuilder = ImmutableDictionary.CreateBuilder<GamepadAxis, ButtonGraphics>();
-
-        foreach (var graphics in Graphics)
-        {
-            switch (graphics.InputButton.Source)
-            {
-                case InputSource.None:
-                    GameLogger.Error($"Input button source is None for {graphics.InputButton}");
-                    break;
-                case InputSource.Keyboard:
-                    if (graphics.InputButton.Keyboard is Keys key)
-                    {
-                        keysBuilder.Add(key, graphics);
-                    }
-                    else
-                    {
-                        GameLogger.Error($"Input source/button mismatch is not a keyboard key for {graphics.InputButton}");
-                    }
-                    break;
-                case InputSource.Mouse:
-                    if (graphics.InputButton.Mouse is MouseButtons mouseButton)
-                    {
-                        mouseButtonsBuilder.Add(mouseButton, graphics);
-                    }
-                    else
-                    {
-                        GameLogger.Error($"Input source/button mismatch is not a mouse button for {graphics.InputButton}");
-                    }
-                    break;
-                case InputSource.Gamepad:
-                    if (graphics.InputButton.Gamepad is Buttons button)
-                    {
-                        buttonsBuilder.Add(button, graphics);
-                    }
-                    else
-                    {
-                        GameLogger.Error($"Input source/button mismatch is not a gamepad button for {graphics.InputButton}");
-                    }
-                    break;
-                case InputSource.GamepadAxis:
-                    if (graphics.InputButton.Axis is GamepadAxis axis)
-                    {
-                        gamepadAxisBuilder.Add(axis, graphics);
-                    }
-                    else
-                    {
-                        GameLogger.Error($"Input source/button mismatch is not a gamepad axis for {graphics.InputButton}");
-                    }
-                    break;
-            }
-        }
-        _graphicsCacheKeys = keysBuilder.ToImmutable();
-        _graphicsCacheButtons = buttonsBuilder.ToImmutable();
-        _graphicsCacheMouseButtons = mouseButtonsBuilder.ToImmutable();
-        _graphicsCacheGamepadAxis = gamepadAxisBuilder.ToImmutable();
-    }
+    public Portrait KeyboardDefault = new();
+    public Portrait GamepadDefault = new();
+    public Portrait MouseDefault = new();
+    public Portrait GamepadAxisDefault = new();
 
     public readonly struct InputInformation
     {
--- src/Murder/Services/InputServices.cs
+++ src/Murder/Services/InputServices.cs
@@ -1,4 +1,5 @@
-﻿using Murder.Assets;
+﻿using Microsoft.Xna.Framework.Input;
+using Murder.Assets;
 using Murder.Assets.Graphics;
 using Murder.Core;
 using Murder.Core.Geometry;
@@ -9,6 +10,7 @@ using Murder.Services.Info;
 using Murder.Utilities;
 using System.Collections.Immutable;
 using System.Numerics;
+using static Murder.Assets.InputInformationAsset;
 
 namespace Murder.Services;
 
@@ -61,6 +63,7 @@ public static class InputServices
 
         return new GenericMenuInfo<InputMenuOption>(builder.ToArray());
     }
+
 
     public static DrawMenuInfo DrawBindingsMenu(Batch2D batch,
         in Point position,
@@ -142,7 +145,7 @@ public static class InputServices
                 // Draw each individual icon
                 foreach (var inputButton in virtualButton.Buttons)
                 {
-                    (Portrait? icon, string? extraText) = infoBindings.GetGraphicsFor(inputButton);
+                    (Portrait? icon, string? extraText) = InputServices.GetGraphicsFor(inputButton);
                     Vector2 iconPosition = new Vector2(labelPosition.X + x, labelPosition.Y - 4);
 
                     if (icon != null)
@@ -197,4 +200,161 @@ public static class InputServices
             MaximumSelectionWidth = Calculator.RoundToInt(totalMenuWidth / collumns)
         };
     }
+
+
+    public static (Portrait?, string?) GetGraphicsFor(InputButton key)
+    {
+        if (Game.Data.TryGetAsset<InputInformationAsset>(Game.Profile.InputInformation) is not InputInformationAsset inputInformationAsset)
+        {
+            GameLogger.Error("Input information not set in the GameProfile asset!");
+            return (null, null);
+        }
+
+        // TODO: Detect changes in the asset and rebuild the cache if needed
+        if (_graphicsCacheKeys == null)
+        {
+            InputServices.BuildGraphicsCache(inputInformationAsset);
+        }
+
+        switch (key.Source)
+        {
+            case InputSource.Keyboard:
+                if (key.Keyboard is Keys keyCode && _graphicsCacheKeys!.TryGetValue(keyCode, out var graphicsIndex))
+                {
+                    var graphics = inputInformationAsset.Graphics[graphicsIndex];
+                    return (graphics.Icon, graphics.Text);
+                }
+                else
+                {
+                    return (inputInformationAsset.KeyboardDefault, key.Keyboard.ToString());
+                }
+
+            case InputSource.Mouse:
+                if (key.Mouse is MouseButtons mouseButton && _graphicsCacheMouseButtons!.TryGetValue(mouseButton, out var mouseGraphicsIndex))
+                {
+                    var mouseGraphics = inputInformationAsset.Graphics[mouseGraphicsIndex];
+                    return (mouseGraphics.Icon, mouseGraphics.Text);
+                }
+                else
+                {
+                    return (inputInformationAsset.MouseDefault, key.Mouse.ToString());
+                }
+            case InputSource.Gamepad:
+                if (key.Gamepad is Buttons button && _graphicsCacheButtons!.TryGetValue(button, out var buttonGraphicsIndex))
+                {
+                    var buttonGraphics = inputInformationAsset.Graphics[buttonGraphicsIndex];
+                    return (buttonGraphics.Icon, buttonGraphics.Text);
+                }
+                else
+                {
+                    return (inputInformationAsset.GamepadDefault, key.Gamepad.ToString());
+                }
+            case InputSource.GamepadAxis:
+                if (key.Axis is GamepadAxis axis && _graphicsCacheGamepadAxis!.TryGetValue(axis, out var axisGraphicsIndex))
+                {
+                    var axisGraphics = inputInformationAsset.Graphics[axisGraphicsIndex];
+                    return (axisGraphics.Icon, axisGraphics.Text);
+                }
+                else
+                {
+                    return (inputInformationAsset.GamepadAxisDefault, key.Axis.ToString());
+                }
+            default:
+                GameLogger.Warning($"Input button source for {key} is invalid");
+                return (null, null);
+        }
+    }
+
+    private static ImmutableDictionary<int, int>? _buttonsCache = null;
+    private static ImmutableDictionary<Keys, int>? _graphicsCacheKeys = null;
+    private static ImmutableDictionary<Buttons, int>? _graphicsCacheButtons = null;
+    private static ImmutableDictionary<MouseButtons, int>? _graphicsCacheMouseButtons = null;
+    private static ImmutableDictionary<GamepadAxis, int>? _graphicsCacheGamepadAxis = null;
+
+    private static void BuildGraphicsCache(InputInformationAsset asset)
+    {
+        var keysBuilder = ImmutableDictionary.CreateBuilder<Keys, int>();
+        var buttonsBuilder = ImmutableDictionary.CreateBuilder<Buttons, int>();
+        var mouseButtonsBuilder = ImmutableDictionary.CreateBuilder<MouseButtons, int>();
+        var gamepadAxisBuilder = ImmutableDictionary.CreateBuilder<GamepadAxis, int>();
+
+        for (int i = 0; i < asset.Graphics.Length; i++)
+        {
+            var graphics = asset.Graphics[i];
+
+            switch (graphics.InputButton.Source)
+            {
+                case InputSource.None:
+                    GameLogger.Error($"Input button source is None for {graphics.InputButton}");
+                    break;
+                case InputSource.Keyboard:
+                    if (graphics.InputButton.Keyboard is Keys key)
+                    {
+
+                    }
+                    else
+                    {
+                        GameLogger.Error($"Input source/button mismatch is not a keyboard key for {graphics.InputButton}");
+                    }
+                    break;
+                case InputSource.Mouse:
+                    if (graphics.InputButton.Mouse is MouseButtons mouseButton)
+                    {
+                        mouseButtonsBuilder.Add(mouseButton, i);
+                    }
+                    else
+                    {
+                        GameLogger.Error($"Input source/button mismatch is not a mouse button for {graphics.InputButton}");
+                    }
+                    break;
+                case InputSource.Gamepad:
+                    if (graphics.InputButton.Gamepad is Buttons button)
+                    {
+                        buttonsBuilder.Add(button, i);
+                    }
+                    else
+                    {
+                        GameLogger.Error($"Input source/button mismatch is not a gamepad button for {graphics.InputButton}");
+                    }
+                    break;
+                case InputSource.GamepadAxis:
+                    if (graphics.InputButton.Axis is GamepadAxis axis)
+                    {
+                        gamepadAxisBuilder.Add(axis, i);
+                    }
+                    else
+                    {
+                        GameLogger.Error($"Input source/button mismatch is not a gamepad axis for {graphics.InputButton}");
+                    }
+                    break;
+            }
+        }
+
+        // Build the cache
+        _graphicsCacheKeys = keysBuilder.ToImmutable();
+        _graphicsCacheButtons = buttonsBuilder.ToImmutable();
+        _graphicsCacheMouseButtons = mouseButtonsBuilder.ToImmutable();
+        _graphicsCacheGamepadAxis = gamepadAxisBuilder.ToImmutable();
+    }
+
+    public static InputInformation? GetButtonInfo(int id)
+    {
+        if (Game.Data.TryGetAsset<InputInformationAsset>(Game.Profile.InputInformation) is not InputInformationAsset inputInformationAsset)
+        {
+            GameLogger.Error("Input information not set in the GameProfile asset!");
+            return null;
+        }
+
+        // TODO: This should be cached
+        foreach (var buttonInfo in inputInformationAsset.Buttons)
+        {
+            if (buttonInfo.ButtonId == id)
+            {
+                return buttonInfo;
+            }
+        }
+
+        return null;
+    }
+
 }
