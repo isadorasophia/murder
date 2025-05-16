ae709ad66e39f88f157264cf52b80ba8ba4a09a5
bonnibel <isaecia@gmail.com>
2025-05-15 23:46:14 -0700

    Sanity check one ambience sound being disabled.

--- src/Murder/Systems/Sound/SoundShapeTrackerSystem.cs
+++ src/Murder/Systems/Sound/SoundShapeTrackerSystem.cs
@@ -75,6 +75,7 @@ public abstract class SoundShapeTrackerSystem : IFixedUpdateSystem, IReactiveSys
                 foreach (SoundEventIdInfo info in ambience.Events)
                 {
                     SoundServices.Stop(info.Id, fadeOut: true, e.EntityId);
+                    SoundServices.Stop(info.Id, fadeOut: true, -1); // can this backfire? o_O
                 }
             }
         }
