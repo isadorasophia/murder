15e841b00c6db03c60c9288838f972b16b0393ad
bonnibel <isaecia@gmail.com>
2025-06-30 14:00:18 -0700

    Add unit tests for murder.

---
+++ src/Murder.Tests/Clue.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Murder.Core.Geometry;
+using Murder.Services;
+
+namespace Murder.Tests;
+
+[TestClass]
+public class Clue
+{
+    [TestMethod]
+    public void TestCircleVsCircle()
+    {
+        CircleShape c1 = new(2, new(0, 0));
+        CircleShape c2 = new(2, new(1, 1));
+
+        bool collides = PhysicsServices.CollidesWith(c1, Point.Zero, c2, Point.Zero);
+        Assert.AreEqual(true, collides);
+    }
+}
