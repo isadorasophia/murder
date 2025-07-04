ca063601ad4d3a684831e1bd667f8d3aaa60349b
bonnibel <isaecia@gmail.com>
2025-07-04 16:26:45 -0700

    ADd new interaction system.

---
+++ src/Murder/Systems/Interactions/InteractOnActivateSystem.cs
@@ -0,0 +1,27 @@
+﻿using Bang;
+using Bang.Contexts;
+using Bang.Entities;
+using Bang.Systems;
+using Murder.Components;
+using System.Collections.Immutable;
+
+namespace Murder.Systems;
+
+[Filter(typeof(InteractOnActivateComponent))]
+[Watch(typeof(InteractOnActivateComponent))]
+internal class InteractOnActivateSystem : IReactiveSystem
+{
+    public void OnAdded(World world, ImmutableArray<Entity> entities) { }
+
+    public void OnModified(World world, ImmutableArray<Entity> entities) { }
+
+    public void OnRemoved(World world, ImmutableArray<Entity> entities) { }
+
+    public void OnActivated(World world, ImmutableArray<Entity> entities) 
+    {
+        foreach (Entity e in entities)
+        {
+            e.SendInteractMessage(e);
+        }
+    }
+}
