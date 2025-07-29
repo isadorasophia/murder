224752abb340db7e6282cf5b4574bb1d6e1d235b
bonnibel <isaecia@gmail.com>
2025-07-29 14:27:20 -0700

    Add new custom interactions on failure.

--- src/Murder/Components/Effects/OnlyApplyOnRuleComponent.cs
+++ src/Murder/Components/Effects/OnlyApplyOnRuleComponent.cs
@@ -1,4 +1,5 @@
 ﻿using Bang.Components;
+using Bang.Interactions;
 using Murder.Core;
 using Murder.Core.Dialogs;
 using System.Collections.Immutable;
@@ -11,7 +12,9 @@ public readonly struct OnlyApplyOnRuleComponent : IComponent
     /// List of requirements which will trigger the interactive component within the same entity.
     /// </summary>
     public readonly ImmutableArray<CriterionNode> Requirements = [];
-    public readonly ImmutableArray<ICondition> Conditions = [];
+    public readonly ImmutableArray<ICondition> Conditions { get; init; } = [];
+
+    public readonly IInteractiveComponent? OnFailure { get; init; } = null;
 
     public OnlyApplyOnRuleComponent() { }
 }
---
+++ src/Murder/Components/Effects/OnlyTriggerInteractionOnComponent.cs
@@ -0,0 +1,15 @@
+﻿using Bang.Components;
+using Bang.Interactions;
+using Murder.Core;
+using System.Collections.Immutable;
+
+namespace Murder.Component;
+
+public readonly struct OnlyTriggerInteractionOnComponent : IComponent
+{
+    public readonly ImmutableArray<ICondition> Conditions { get; init; } = [];
+
+    public readonly IInteractiveComponent? OnFailure { get; init; } = null;
+
+    public OnlyTriggerInteractionOnComponent() { }
+}
