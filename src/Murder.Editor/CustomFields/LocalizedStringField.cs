995e749cc40a624bc9b926e20e5c7a3c543eeb95
bonnibel <isaecia@gmail.com>
2025-08-23 11:40:43 -0700

    minor tweak on localized string being null text.

--- src/Murder.Editor/CustomFields/LocalizedStringField.cs
+++ src/Murder.Editor/CustomFields/LocalizedStringField.cs
@@ -113,11 +113,13 @@ internal class LocalizedStringField : CustomField
         }
         else
         {
-            if (text == null)
+            if (text is null)
             {
+                modified = true;
                 text = string.Empty;
             }
-            modified = ImGui.InputText($"##{data.Guid}", ref text, 2048);
+
+            modified |= ImGui.InputText($"##{data.Guid}", ref text, 2048);
         }
 
         if (modified)
