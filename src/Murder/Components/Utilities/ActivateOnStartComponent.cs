e899f38f11ba0b6d2be7773ec9ddff378412a7d3
saint11 <saintjust@gmail.com>
2025-10-03 20:39:12 -0700

    added constructor to activate on start

--- src/Murder/Components/Utilities/ActivateOnStartComponent.cs
+++ src/Murder/Components/Utilities/ActivateOnStartComponent.cs
@@ -2,8 +2,17 @@
 
 namespace Murder.Components.Utilities;
 
-public readonly struct ActivateOnStartComponent() : IComponent
+public readonly struct ActivateOnStartComponent : IComponent
 {
     public readonly AfterInteractRule After = AfterInteractRule.Always;
     public readonly bool DeactivateInstead = false;
+    public ActivateOnStartComponent()
+    {
+        
+    }
+    public ActivateOnStartComponent(AfterInteractRule after, bool deactivateInstead)
+    {
+        After = after;
+        DeactivateInstead = deactivateInstead;
+    }
 }
