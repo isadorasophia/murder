9ac823d7bcec4d3a7a609da0c8994e8cfdd69fef
saint11 <saintjust@gmail.com>
2025-10-28 16:47:07 -0700

    added comment in friction component

--- src/Murder/Components/Physics/FrictionComponent.cs
+++ src/Murder/Components/Physics/FrictionComponent.cs
@@ -5,7 +5,9 @@ namespace Murder.Components
     public readonly struct FrictionComponent : IComponent
     {
         public readonly float Amount;
-
+        /// <summary>
+        /// High friction means stopping fast
+        /// </summary>
         public FrictionComponent(float amount) : this()
         {
             Amount = amount;
