407986ca1caaadf72a11f41ce8f315c5d6d6d338
bonnibel <isaecia@gmail.com>
2025-11-20 12:09:03 -0800

    Add helper components.

---
+++ src/Murder/Messages/RevertMessage.cs
@@ -0,0 +1,8 @@
+﻿using Bang.Components;
+
+namespace Murder.Messages;
+
+/// <summary>
+/// Generic struct for reverting an action.
+/// </summary>
+public readonly struct RevertMessage : IMessage { }
