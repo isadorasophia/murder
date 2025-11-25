f04da260392094c39775b170c0d83f6df6112277
bonnibel <isaecia@gmail.com>
2025-11-25 14:03:55 -0800

    Add effects component.

---
+++ src/Murder/Components/World/ImmediatelyApplyEffectsComponent.cs
@@ -0,0 +1,17 @@
+﻿using Bang.Components;
+using Murder.Utilities.Attributes;
+
+namespace Murder.Components;
+
+/// <summary>
+/// Component that will disable effects.
+/// </summary>
+[RuntimeOnly]
+[Unique]
+public readonly struct ImmediatelyApplyEffectsComponent : IComponent
+{
+    public readonly float AddedAt = 0;
+
+    public ImmediatelyApplyEffectsComponent() =>
+        AddedAt = Game.NowUnscaled;
+}
