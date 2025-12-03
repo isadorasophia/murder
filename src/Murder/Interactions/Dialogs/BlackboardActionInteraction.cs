cea1e21a27ed75267056f1eb11530503a08c94ef
bonnibel <isaecia@gmail.com>
2025-12-03 13:11:36 -0800

    Remove triggeronlyonce from interaction blackboard.

--- src/Murder/Interactions/Dialogs/BlackboardActionInteraction.cs
+++ src/Murder/Interactions/Dialogs/BlackboardActionInteraction.cs
@@ -13,21 +13,12 @@ namespace Murder.Interactions
         [ShowInEditor]
         private readonly DialogAction _action = new();
 
-        [ShowInEditor]
-        [Tooltip("Whether this entity will only be triggered once.")]
-        private readonly bool _triggeredOnlyOnce = false;
-
         public BlackboardActionInteraction() { }
 
         public void Interact(World world, Entity interactor, Entity? interacted)
         {
             BlackboardTracker tracker = MurderSaveServices.CreateOrGetSave().BlackboardTracker;
             MurderSaveServices.DoAction(tracker, _action);
-
-            if (_triggeredOnlyOnce)
-            {
-                interacted?.Destroy();
-            }
         }
     }
 }
