1cdf01ef7ffa188d117b573b1f6b036955547404
bonnibel <isaecia@gmail.com>
2025-12-06 01:02:20 -0800

    Improve support for children activation.

--- src/Murder/Interactions/DeactivateChildInteraction.cs
+++ src/Murder/Interactions/DeactivateChildInteraction.cs
@@ -2,35 +2,41 @@
 using Bang.Entities;
 using Bang.Interactions;
 using Murder.Attributes;
-using Murder.Diagnostics;
+using Murder.Services;
 using Murder.Utilities.Attributes;
 using System.Collections.Immutable;
 
-namespace Murder.Interactions
-{
-    public readonly struct DeactivateChildInteraction() : IInteraction
-    {
-        [ChildId, Serialize, ShowInEditor]
-        private readonly ImmutableArray<string> _child = [];
+namespace Murder.Interactions;
 
-        public void Interact(World world, Entity interactor, Entity? interacted)
+public readonly struct DeactivateChildInteraction() : IInteraction
+{
+    [ChildId, Serialize, ShowInEditor]
+    private readonly ImmutableArray<string> _child = [];
+
+    [Serialize]
+    private readonly bool _activateInstead = false;
+
+    public void Interact(World world, Entity interactor, Entity? interacted)
+    {
+        if (interacted is null)
         {
-            if (interacted is Entity target)
-            {
-                target = target.TryFetchParent() ?? target;
-                foreach (var child in _child)
-                {
-                    if (target.TryFetchChild(child) is Entity c)
-                    {
-                        c.Deactivate();
-                    }
-                }
-            }
+            return;
         }
 
-        private static void LogFailedTargetWarning()
+        Entity target = EntityServices.FindRootEntity(interacted);
+        foreach (string child in _child)
         {
-            GameLogger.Warning("DisableChildInteraction has no valid target");
+            if (target.TryFetchChild(child) is Entity c)
+            {
+                if (_activateInstead)
+                {
+                    c.Activate();
+                }
+                else
+                {
+                    c.Deactivate();
+                }
+            }
         }
     }
 }
