c46e88a3297a91e21eed1a855c5be7a7df32d0f2
bonnibel <isaecia@gmail.com>
2025-12-25 23:28:32 -0800

    Fix attribute.

--- src/Murder/Components/Agents/OverrideYOffsetComponent.cs
+++ src/Murder/Components/Agents/OverrideYOffsetComponent.cs
@@ -1,9 +1,12 @@
-﻿using Bang.Components;
+﻿using Bang;
+using Bang.Components;
+using Murder.Attributes;
 using Murder.Utilities.Attributes;
+using System.Text.Json.Serialization;
 
 namespace Murder.Components;
 
-[RuntimeOnly]
+[DoNotPersistOnSave]
 public readonly struct OverrideYOffsetComponent : IComponent
 {
     public readonly float YOffset = 0;
