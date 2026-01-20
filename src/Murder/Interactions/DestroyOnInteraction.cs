50fbfa6b5684a8b19434279c4d851336ce871961
bonnibel <isaecia@gmail.com>
2026-01-19 22:47:11 -0800

    Implement DestroyOnInteraction

---
+++ src/Murder/Interactions/DestroyOnInteraction.cs
@@ -0,0 +1,23 @@
+﻿using Bang;
+using Bang.Entities;
+using Bang.Interactions;
+using Murder.Utilities;
+
+namespace Murder.Interactions;
+
+public readonly struct DestroyOnInteraction : IInteraction
+{
+    public readonly TargetEntity Target = TargetEntity.Self;
+
+    public DestroyOnInteraction() { }
+
+    public void Interact(World world, Entity interactor, Entity? interacted)
+    {
+        switch (Target)
+        {
+            case TargetEntity.Self:
+                interacted?.Destroy();
+                break;
+        }
+    }
+}
