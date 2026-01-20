4781996ed8c3807e1772fd259dc16782f682c71b
bonnibel <isaecia@gmail.com>
2026-01-20 12:40:31 -0800

    Change fatal damage.

--- src/Murder/Messages/FatalDamageMessage.cs
+++ src/Murder/Messages/FatalDamageMessage.cs
@@ -1,6 +1,4 @@
 ﻿using Bang.Components;
-using System.Collections.Immutable;
-using System.Numerics;
 
 namespace Murder.Messages
 {
@@ -9,17 +7,24 @@ namespace Murder.Messages
     /// </summary>
     public readonly struct FatalDamageMessage : IMessage
     {
-        public readonly Vector2 FromPosition = Vector2.Zero;
-        public readonly int Amount;
-        public readonly int DamageSourceId = -1;
-        public readonly int AttackedId = -1;
+        /// <summary>
+        /// This is the entity id that caused the damage.
+        /// When environmental or caused by itself, either same value as <see cref="DamagedEntityId"/> or -1.
+        /// </summary>
+        public readonly int FromId = -1;
 
-        public FatalDamageMessage(Vector2 fromPosition, int damageAmount, int source, int attacked)
+        /// <summary>
+        /// This is the entity id that got attacked.
+        /// If -1, assume it's a critical fatal (everything got the fatal damage).
+        /// </summary>
+        public readonly int DamagedEntityId = -1;
+
+        public FatalDamageMessage() { }
+
+        public FatalDamageMessage(int fromId, int damagedEntityId)
         {
-            FromPosition = fromPosition;
-            Amount = damageAmount;
-            DamageSourceId = source;
-            AttackedId = attacked;
+            FromId = fromId;
+            DamagedEntityId = damagedEntityId;
         }
     }
 }
