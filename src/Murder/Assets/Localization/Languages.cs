1e172155e3a32afbf41162ce8812480b3be3454c
bonnibel <isaecia@gmail.com>
2026-02-03 15:53:36 -0800

    Update preferences.

--- src/Murder/Assets/Localization/Languages.cs
+++ src/Murder/Assets/Localization/Languages.cs
@@ -12,18 +12,49 @@
     {
         private static LanguageIdData[]? _all = null;
 
-        public static LanguageIdData[] All => _all ??= [ English, Portuguese, Japanese, Chinese];
+        public static LanguageIdData[] All => _all ??= [
+            English, 
+            Portuguese, 
+            Japanese, 
+            Chinese, 
+            TraditionalChinese, 
+            French, 
+            Italian,
+            German, 
+            SpanishSpain, 
+            SpanishLatam, 
+            Korean, 
+            Russian, 
+            Turkish, 
+            Ukrainian, 
+            Hungarian];
 
-        public static LanguageIdData Get(LanguageId id) =>
-            All[(int)id];
+        public static LanguageIdData? TryGet(LanguageId id)
+        {
+            int index = (int)id;
+            if (index >= All.Length)
+            {
+                return null;
+            }
 
-        public static LanguageIdData Next(LanguageId id) =>
-            All[((int)id + 1) % All.Length];
+            return All[index];
+        }
 
         public static readonly LanguageIdData English = new(LanguageId.English, "en-US");
         public static readonly LanguageIdData Portuguese = new(LanguageId.Portuguese, "pt-BR");
         public static readonly LanguageIdData Japanese = new(LanguageId.Japanese, "ja");
-        public static readonly LanguageIdData Chinese = new(LanguageId.Chinese, "zh");
+        public static readonly LanguageIdData Chinese = new(LanguageId.Chinese, "zh-Hans");
+        public static readonly LanguageIdData TraditionalChinese = new(LanguageId.TraditionalChinese, "zh-Hant");
+        public static readonly LanguageIdData French = new(LanguageId.French, "fr");
+        public static readonly LanguageIdData Italian = new(LanguageId.Italian, "it");
+        public static readonly LanguageIdData German = new(LanguageId.German, "de");
+        public static readonly LanguageIdData SpanishSpain = new(LanguageId.SpanishSpain, "es-ES");
+        public static readonly LanguageIdData SpanishLatam = new(LanguageId.SpanishLatam, "es-LATAM");
+        public static readonly LanguageIdData Korean = new(LanguageId.Korean, "ko");
+        public static readonly LanguageIdData Russian = new(LanguageId.Russian, "ru");
+        public static readonly LanguageIdData Turkish = new(LanguageId.Turkish, "tr");
+        public static readonly LanguageIdData Ukrainian = new(LanguageId.Ukrainian, "uk");
+        public static readonly LanguageIdData Hungarian = new(LanguageId.Hungarian, "hu");
     }
 
     public enum LanguageId
@@ -31,6 +62,17 @@
         English = 0,
         Portuguese = 1,
         Japanese = 2,
-        Chinese = 3
+        Chinese = 3,
+        TraditionalChinese = 4,
+        French = 5,
+        Italian = 6,
+        German = 7,
+        SpanishSpain = 8,
+        SpanishLatam = 9,
+        Korean = 10,
+        Russian = 11,
+        Turkish = 12,
+        Ukrainian = 13,
+        Hungarian = 14
     }
 }
