4d4ce77a5e4040413e625520cec6555ad9627a5f
bonnibel <isaecia@gmail.com>
2026-02-20 14:01:30 -0800

    Add sound tracker.

---
+++ src/Murder/Components/Sound/TrackDistanceWithParameterComponent.cs
@@ -0,0 +1,18 @@
+﻿using Bang.Components;
+using Murder.Core.Dialogs;
+using Murder.Utilities;
+
+namespace Murder.Components;
+
+public readonly struct TrackDistanceWithParameterComponent : IComponent
+{
+    public readonly float MinRange = 30;
+    public readonly float MaxRange = 200;
+
+    public readonly EaseKind EaseKind = EaseKind.Linear;
+    public readonly Fact Parameter = new();
+
+    public readonly ListenerKind Listener = ListenerKind.Camera;
+
+    public TrackDistanceWithParameterComponent() { }
+}
