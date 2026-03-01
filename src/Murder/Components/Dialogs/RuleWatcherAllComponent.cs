53c3e8c14656d4073f11416acc9fa8ad825a6880
bonnibel <isaecia@gmail.com>
2026-03-01 01:49:42 -0800

    Fix call for cleaning up pending triggers.

---
+++ src/Murder/Components/Dialogs/RuleWatcherAllComponent.cs
@@ -0,0 +1,23 @@
+﻿using Bang.Components;
+using Murder.Attributes;
+using Murder.Core.Dialogs;
+using Murder.Services;
+using Murder.Utilities.Attributes;
+
+namespace Murder.Components;
+
+[Unique]
+[RuntimeOnly]
+[DoNotPersistEntityOnSave]
+public readonly struct RuleWatcherAllComponent : IModifiableComponent
+{
+    public void Subscribe(Action notification)
+    {
+        MurderSaveServices.CreateOrGetSave()?.BlackboardTracker.WatchAll(notification, BlackboardKind.All);
+    }
+
+    public void Unsubscribe(Action notification)
+    {
+        MurderSaveServices.TryGetSave()?.BlackboardTracker.ResetAllWatchers(notification, BlackboardKind.All);
+    }
+}
--- src/Murder/Systems/Interactions/InteractOnRuleMatchSystem.cs
+++ src/Murder/Systems/Interactions/InteractOnRuleMatchSystem.cs
@@ -12,203 +12,229 @@ using Murder.Save;
 using Murder.Services;
 using Murder.Utilities;
 using System.Collections.Immutable;
-using System.Diagnostics;
 
-namespace Murder.Systems
+namespace Murder.Systems;
+
+[Filter(typeof(RuleWatcherComponent))]
+[Watch(typeof(RuleWatcherComponent))]
+public class InteractOnRuleMatchSystem : IStartupSystem, IReactiveSystem
 {
-    [Filter(typeof(RuleWatcherComponent))]
-    [Watch(typeof(RuleWatcherComponent))]
-    public class InteractOnRuleMatchSystem : IStartupSystem, IReactiveSystem
+    public void Start(Context context)
     {
-        public void Start(Context context)
-        {
-            _ = context.World.AddEntity(new RuleWatcherComponent());
-        }
+        _ = context.World.AddEntity(new RuleWatcherComponent());
+    }
 
-        public void OnAdded(World world, ImmutableArray<Entity> entities)
-        {
-            CheckAndTriggerRules(world, modified: false);
-        }
+    public void OnAdded(World world, ImmutableArray<Entity> entities)
+    {
+        CheckAndTriggerRules(world, modified: false);
+    }
 
-        public void OnModified(World world, ImmutableArray<Entity> entities)
-        {
-            CheckAndTriggerRules(world, modified: true);
-        }
+    public void OnModified(World world, ImmutableArray<Entity> entities)
+    {
+        CheckAndTriggerRules(world, modified: true);
+    }
 
-        public void OnRemoved(World world, ImmutableArray<Entity> entities) 
+    public void OnRemoved(World world, ImmutableArray<Entity> entities) 
+    {
+        foreach (Entity e in entities)
         {
-            foreach (Entity e in entities)
+            e.RemoveRuleMatched();
+        }
+    }
+
+    private void CheckAndTriggerRules(World world, bool modified)
+    {
+        SaveData save = MurderSaveServices.CreateOrGetSave();
+        BlackboardTracker tracker = save.BlackboardTracker;
+
+        // Fetch all entities which might be affected by a rule change.
+        ImmutableArray<Entity> interactives = world.GetEntitiesWith(typeof(InteractOnRuleMatchComponent));
+        foreach (Entity e in interactives)
+        {
+            if (e.IsDestroyed)
             {
-                e.RemoveRuleMatched();
+                continue;
             }
-        }
 
-        private void CheckAndTriggerRules(World world, bool modified)
-        {
-            SaveData save = MurderSaveServices.CreateOrGetSave();
-            BlackboardTracker tracker = save.BlackboardTracker;
+            // Match each of its requirements.
+            InteractOnRuleMatchComponent ruleComponent = e.GetInteractOnRuleMatch();
 
-            // Fetch all entities which might be affected by a rule change.
-            ImmutableArray<Entity> interactives = world.GetEntitiesWith(typeof(InteractOnRuleMatchComponent));
-            foreach (Entity e in interactives)
+            bool matched = CheckEntity(world, tracker, ruleComponent, modified);
+            bool triggered = false;
+
+            if (e.HasRuleMatched())
             {
-                if (e.IsDestroyed)
+                if (!matched)
+                {
+                    e.RemoveRuleMatched();
+                }
+                else
                 {
                     continue;
                 }
+            }
 
-                // Match each of its requirements.
-                InteractOnRuleMatchComponent ruleComponent = e.GetInteractOnRuleMatch();
+            if (matched)
+            {
+                e.SendInteractMessage(e);
+                e.SetRuleMatched();
 
-                bool matched = CheckEntity(world, tracker, ruleComponent, modified);
-                bool triggered = false;
+                triggered = true;
+            }
 
-                if (e.HasRuleMatched())
+            if (e.TryGetPickEntityToAddOnStart() is PickEntityToAddOnStartComponent pickEntityOnStart)
+            {
+                Guid target = matched ? pickEntityOnStart.OnMatchPrefab : pickEntityOnStart.OnNotMatchPrefab;
+
+                e.SetInteractive(new InteractiveComponent<AddEntityOnInteraction>(new(target)));
+                e.SendInteractMessage(e);
+
+                triggered = true;
+            }
+
+            if (triggered)
+            {
+                CleanupRuleMatchEntity(world, e, ruleComponent);
+            }
+        }
+
+        // Fetch all entities which might be affected by a rule change.
+        interactives = world.GetEntitiesWith(typeof(InteractOnRuleMatchCollectionComponent));
+        foreach (Entity e in interactives)
+        {
+            if (e.IsDestroyed)
+            {
+                continue;
+            }
+
+            InteractOnRuleMatchCollectionComponent ruleCollection = e.GetInteractOnRuleMatchCollection();
+            ImmutableArray<InteractOnRuleMatchComponent> requirements = ruleCollection.Requirements;
+
+            bool matched = false;
+
+            Stack<int> triggeredRequirements = new();
+            for (int i = 0; i < requirements.Length; ++i)
+            {
+                if (CheckEntity(world, tracker, requirements[i], modified))
                 {
-                    if (!matched)
-                    {
-                        e.RemoveRuleMatched();
-                    }
-                    else
-                    {
-                        continue;
-                    }
-                }
-
-                if (matched)
-                {
-                    e.SendInteractMessage(e);
-                    e.SetRuleMatched();
-
-                    triggered = true;
-                }
-
-                if (e.TryGetPickEntityToAddOnStart() is PickEntityToAddOnStartComponent pickEntityOnStart)
-                {
-                    Guid target = matched ? pickEntityOnStart.OnMatchPrefab : pickEntityOnStart.OnNotMatchPrefab;
-
-                    e.SetInteractive(new InteractiveComponent<AddEntityOnInteraction>(new(target)));
-                    e.SendInteractMessage(e);
-
-                    triggered = true;
-                }
-
-                if (triggered)
-                {
-                    CleanupRuleMatchEntity(world, e, ruleComponent);
+                    matched = true;
+                    triggeredRequirements.Push(i);
                 }
             }
 
-            // Fetch all entities which might be affected by a rule change.
-            interactives = world.GetEntitiesWith(typeof(InteractOnRuleMatchCollectionComponent));
-            foreach (Entity e in interactives)
+            if (e.HasRuleMatched())
             {
-                if (e.IsDestroyed)
+                if (!matched)
+                {
+                    e.RemoveRuleMatched();
+                }
+                else
                 {
                     continue;
                 }
-
-                InteractOnRuleMatchCollectionComponent ruleCollection = e.GetInteractOnRuleMatchCollection();
-                ImmutableArray<InteractOnRuleMatchComponent> requirements = ruleCollection.Requirements;
-
-                bool matched = false;
-
-                Stack<int> triggeredRequirements = new();
-                for (int i = 0; i < requirements.Length; ++i)
-                {
-                    if (CheckEntity(world, tracker, requirements[i], modified))
-                    {
-                        matched = true;
-                        triggeredRequirements.Push(i);
-                    }
-                }
-
-                if (e.HasRuleMatched())
-                {
-                    if (!matched)
-                    {
-                        e.RemoveRuleMatched();
-                    }
-                    else
-                    {
-                        continue;
-                    }
-                }
-
-                if (matched)
-                {
-                    e.SendInteractMessage(e);
-                    e.SetRuleMatched();
-                }
-
-                if (triggeredRequirements.Count == 0)
-                {
-                    continue;
-                }
-
-                while (triggeredRequirements.TryPeek(out int next))
-                {
-                    InteractOnRuleMatchComponent rule = requirements[next];
-                    switch (rule.AfterInteraction)
-                    {
-                        case AfterInteractRule.InteractOnlyOnce:
-                            requirements = requirements.RemoveAt(next);
-
-                            if (rule.InteractOn == InteractOn.AddedOrModified)
-                            {
-                                // Right now, this will remove the entity from the save.
-                                // Maybe we want to do something else?
-                                e.RecordAndMaybeDestroy(world, destroy: false);
-                            }
-
-                            break;
-
-                        case AfterInteractRule.RemoveComponent:
-                            e.RemoveInteractOnRuleMatch();
-                            break;
-
-                        case AfterInteractRule.Destroy:
-                            e.Destroy();
-                            break;
-                    }
-
-                    triggeredRequirements.Pop();
-                }
-
-                e.SetInteractOnRuleMatchCollection(requirements);
             }
 
-            tracker.ResetPendingTriggers();
-        }
-
-        private bool CheckEntity(World world, BlackboardTracker tracker, InteractOnRuleMatchComponent ruleComponent, bool wasRuleModified)
-        {
-            if (ruleComponent.InteractOn == InteractOn.Modified && !wasRuleModified)
+            if (matched)
             {
-                // Do not trigger when rule watcher has been added (probably due to a map load).
-                return false;
+                e.SendInteractMessage(e);
+                e.SetRuleMatched();
             }
 
-            return BlackboardHelpers.Match(world, tracker, ruleComponent.Requirements);
-        }
-
-        private void CleanupRuleMatchEntity(World world, Entity e, InteractOnRuleMatchComponent rule)
-        {
-            switch (rule.AfterInteraction)
+            if (triggeredRequirements.Count == 0)
             {
-                case AfterInteractRule.InteractOnlyOnce:
-                    e.RecordAndMaybeDestroy(world, destroy: false);
-                    e.RemoveInteractOnRuleMatch();
-                    break;
-
-                case AfterInteractRule.RemoveComponent:
-                    e.RemoveInteractOnRuleMatch();
-                    break;
-
-                case AfterInteractRule.Destroy:
-                    e.Destroy();
-                    break;
+                continue;
             }
+
+            while (triggeredRequirements.TryPeek(out int next))
+            {
+                InteractOnRuleMatchComponent rule = requirements[next];
+                switch (rule.AfterInteraction)
+                {
+                    case AfterInteractRule.InteractOnlyOnce:
+                        requirements = requirements.RemoveAt(next);
+
+                        if (rule.InteractOn == InteractOn.AddedOrModified)
+                        {
+                            // Right now, this will remove the entity from the save.
+                            // Maybe we want to do something else?
+                            e.RecordAndMaybeDestroy(world, destroy: false);
+                        }
+
+                        break;
+
+                    case AfterInteractRule.RemoveComponent:
+                        e.RemoveInteractOnRuleMatch();
+                        break;
+
+                    case AfterInteractRule.Destroy:
+                        e.Destroy();
+                        break;
+                }
+
+                triggeredRequirements.Pop();
+            }
+
+            e.SetInteractOnRuleMatchCollection(requirements);
         }
+    }
+
+    private bool CheckEntity(World world, BlackboardTracker tracker, InteractOnRuleMatchComponent ruleComponent, bool wasRuleModified)
+    {
+        if (ruleComponent.InteractOn == InteractOn.Modified && !wasRuleModified)
+        {
+            // Do not trigger when rule watcher has been added (probably due to a map load).
+            return false;
+        }
+
+        return BlackboardHelpers.Match(world, tracker, ruleComponent.Requirements);
+    }
+
+    private void CleanupRuleMatchEntity(World world, Entity e, InteractOnRuleMatchComponent rule)
+    {
+        switch (rule.AfterInteraction)
+        {
+            case AfterInteractRule.InteractOnlyOnce:
+                e.RecordAndMaybeDestroy(world, destroy: false);
+                e.RemoveInteractOnRuleMatch();
+                break;
+
+            case AfterInteractRule.RemoveComponent:
+                e.RemoveInteractOnRuleMatch();
+                break;
+
+            case AfterInteractRule.Destroy:
+                e.Destroy();
+                break;
+        }
+    }
+}
+
+[Filter(typeof(RuleWatcherAllComponent))]
+[Watch(typeof(RuleWatcherAllComponent))]
+public class RuleCleanupSystem : IStartupSystem, IReactiveSystem
+{
+    public void Start(Context context)
+    {
+        _ = context.World.AddEntity(new RuleWatcherAllComponent());
+    }
+
+    public void OnAdded(World world, ImmutableArray<Entity> entities)
+    {
+        Cleanup();
+    }
+
+    public void OnModified(World world, ImmutableArray<Entity> entities)
+    {
+        Cleanup();
+    }
+
+    public void OnRemoved(World world, ImmutableArray<Entity> entities) { }
+
+    private void Cleanup()
+    {
+        SaveData save = MurderSaveServices.CreateOrGetSave();
+        BlackboardTracker tracker = save.BlackboardTracker;
+
+        tracker.ResetPendingTriggers();
     }
 }
