7a7b38bd7ea47f5dbe98db593666c1fa60ab4612
bonnibel <isaecia@gmail.com>
2026-03-02 14:17:18 -0800

    Nit change on imports.

--- src/Murder/Components/Utilities/CreatedAtComponent.cs
+++ src/Murder/Components/Utilities/CreatedAtComponent.cs
@@ -1,11 +1,6 @@
 ﻿using Bang.Components;
 using Murder.Attributes;
 using Murder.Utilities.Attributes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Murder.Components;
 
