8c40108a55b1926d8d0daa06ec8de482b208c419
bonnibel <isaecia@gmail.com>
2026-05-21 00:07:14 -0700

    Fix serialization for SpritePausedComponent

--- src/Murder/Components/Graphics/SpritePauseComponent.cs
+++ src/Murder/Components/Graphics/SpritePauseComponent.cs
@@ -1,12 +1,17 @@
-﻿
-
-using Bang.Components;
+﻿using Bang.Components;
+using Murder.Utilities.Attributes;
 
 namespace Murder.Components.Graphics;
 
+[RuntimeOnly]
 public readonly struct SpritePausedComponent : IComponent
 {
     public readonly float PausedAt;
+
+    public SpritePausedComponent()
+    {
+        PausedAt = Game.Now;
+    }
 
     public SpritePausedComponent(float pausedAt)
     {
