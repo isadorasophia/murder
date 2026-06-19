d4026e4d5ce8d09917c8e97e90a099750d3f83d6
bonnibel <isaecia@gmail.com>
2026-06-19 00:18:09 -0700

    Fix enum fields on editor.

--- src/Murder.Editor/CustomFields/EnumField.cs
+++ src/Murder.Editor/CustomFields/EnumField.cs
@@ -33,15 +33,15 @@ namespace Murder.Editor.CustomFields
 
             bool modified = false;
 
-            if (AttributeExtensions.TryGetAttribute<SearchAttribute>(member, out var _searchAttribute) || Enum.GetValues(t).Length > 10)
-            {
-                modified = ImGuiHelpers.DrawEnumFieldWithSearch($"##{member.Name}", t, ref intValue);
-                return (modified, Enum.ToObject(t, intValue));
-            }
-
             if (Attribute.IsDefined(t, typeof(FlagsAttribute)))
             {
                 modified |= ImGuiHelpers.DrawEnumFieldAsDropdown(member.Name, t, ref intValue);
+                return (modified, Enum.ToObject(t, intValue));
+            }
+
+            if (AttributeExtensions.TryGetAttribute<SearchAttribute>(member, out var _searchAttribute) || Enum.GetValues(t).Length > 10)
+            {
+                modified = ImGuiHelpers.DrawEnumFieldWithSearch($"##{member.Name}", t, ref intValue);
                 return (modified, Enum.ToObject(t, intValue));
             }
 
